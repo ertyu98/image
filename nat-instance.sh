18f48a903aa88fa94b75d5ad82b2faf3b65e6866
ertyu98 <157345680+ertyu98@users.noreply.github.com>
2024-03-29 15:55:43 +0900

    Update nat-instance.sh

--- nat-instance.sh
+++ nat-instance.sh
@@ -4,5 +4,6 @@ sudo sysctl -p /etc/sysctl.conf
 sudo yum install -y iptables-services
 sudo systemctl start iptables.service
 sudo systemctl enable iptables.service
+sudo iptables -F
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo service iptables save
