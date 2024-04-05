2e53858c02fa1337d19abe59fe6f2369b99b8a0d
ertyu98 <157345680+ertyu98@users.noreply.github.com>
2024-04-05 10:39:09 +0900

    Update web-server.sh

--- web-server.sh
+++ web-server.sh
@@ -4,9 +4,9 @@ instance_id=$(ec2-metadata -i)
 private_ip=$(ec2-metadata -o)
 yum install -y httpd
 systemctl start httpd
-systenctl enable httpd
+systemctl enable httpd
 cat <<EOF > /var/www/html/index.html
-<h1>Welcome to Web Server.</h1>
+<h1>Welcome to Saopaulo Web Server.</h1>
 <h3>${availability_zone}</h3>
 <h3>${instance_id}</h3>
 <h3>${private_ip}</h3>
